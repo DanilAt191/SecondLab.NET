04a7b74ec49e699da00b96cf141aa0abbf403b34
DanilAt191 <61827117+DanilAt191@users.noreply.github.com>
2020-03-22 11:47:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLab.NET
+{ 
+    class Program
+    {
+        class Okrujnost
+        {
+            private double rad;
+            public double Rad { get => rad; set => rad = value;}
+            public double Area()
+            {
+                return Math.PI*(Rad*Rad) ;
+            }
+
+            public  double Dlina()
+            {
+                return 2 * Math.PI * Rad;
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"Окружность: радиус = {Rad}, площадь = {String.Format("{0:f1}", Area())}, длина окружности = {String.Format("{0:f1}", Dlina())}");
+            }
+        }
+   
+        static void Main(string[] args)
+        {
+            Okrujnost okrujnost = new Okrujnost();
+            okrujnost.Rad = 3;
+            Console.WriteLine($"Площадь: {okrujnost.Area()}");
+            Console.WriteLine($"Длина окружности: {okrujnost.Dlina()}");
+            Console.ReadLine();
+        }
+    }
+}
